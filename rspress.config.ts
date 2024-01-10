533375ead7b4e7165c0bf1b7b03eff3c7c8381ec
dongrentianyu <3465645884@qq.com>
2024-01-10 23:41:49 +0800

    rename A TiddlyWiki of Odyssey to A TiddlyWiki Odyssey
    等下肯定要把文件夹和仓库的名称都要改掉

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,18 +2,24 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  base: "/a-tiddlywiki-of-odyssey",
+  base: "/a-tiddlywiki-odyssey",
   root: path.join(__dirname, 'docs'),
-  title: 'a-tiddlywiki-of-odyssey',
-  description: 'A TiddlyWiki of Odyssey',
+  title: 'a-tiddlywiki-odyssey',
+  description: 'A TiddlyWiki Odyssey',
   icon: '/favicon.ico',
   logo: {
     light: '/favicon.ico',
     dark: '/favicon.ico',
   },
   themeConfig: {
+    outlineTitle: '目录',
+    lastUpdated: true,
+    lastUpdatedText: '最后更新于',
+    prevPageText: '上一篇',
+    nextPageText: '下一篇',
+    sourceCodeText: 'Source',
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/dongrentianyu/a-tiddlywiki-of-odyssey' },
+      { icon: 'github', mode: 'link', content: 'https://github.com/dongrentianyu/a-tiddlywiki-odyssey' },
     ],
   },
 });
